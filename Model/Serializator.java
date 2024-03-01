ad75755ccb62828e9f87432658576c145ba698f6
senseigithub <adriibasti14@gmail.com>
2024-03-01 20:12:16 +0100

    Serializador

---
+++ Model/Serializator.java
@@ -0,0 +1,36 @@
+package Model;
+
+import java.io.*;
+
+public class Serializator {
+    /**
+     *
+     * @param obj
+     * @param filename
+     * @return
+     * @param <T>
+     */
+    public static<T> boolean serializable(T obj,  String filename){
+        boolean result= false;
+        try(ObjectOutputStream oos =new ObjectOutputStream(
+                new FileOutputStream(filename)
+        )) {
+            oos.writeObject(obj);
+            result = true;
+        }catch (IOException e) {
+            //throw new RuntimeException(e);
+        }
+        return result;
+    }
+    public static<T> T desearize(String filename){
+        T result=null;
+        try(ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream(filename)
+        )){
+            result=(T)ois.readObject();
+        }catch(IOException | ClassNotFoundException e){
+            //no hago nada por simplicidad
+        }
+        return result;
+    }
+}
--- Model/Serializer.java
+++
@@ -1,5 +0,0 @@
-package Model;
-
-public class Serializer {
-    //Aqui va el serializador
-}
