ab2d96b3bab192c8afd2227b5875211e563014e2
senseigithub <adriibasti14@gmail.com>
2024-03-06 09:28:22 +0100

    Try Catch

--- Utils/IO.java
+++ Utils/IO.java
@@ -4,14 +4,29 @@ import java.util.Scanner;
 
 public class IO {
     private static Scanner teclado = new Scanner(System.in);
-    public String readString(String message){
+    public String readString(String message) {
         System.out.println(message);
-        return teclado.nextLine();
+        String input = " ";
+        try {
+            input = teclado.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error al leer la cadena.");
+            // Limpia el buffer del scanner para evitar un bucle infinito
+            teclado.nextLine();
+        }
+        return input;
     }
 
-    public int readInt(String message){
+    public int readInt(String message) {
         System.out.println(message);
         int opcion = 0;
-        return teclado.nextInt();
+        try {
+            opcion = teclado.nextInt();
+        } catch (Exception e) {
+            System.out.println("Error: Ingresa un valor entero válido.");
+            // Limpia el buffer del scanner para evitar un bucle infinito
+            teclado.nextLine();
+        }
+        return opcion;
     }
 }
