1fe4fca3fddc2785dc5ebd2a09f301a3cf7ac74f
Stilver <stilvergonzales@gmail.com>
2024-03-06 21:07:41 +0100

    RepoUser terminao

--- Interfaces/Repos/IRepoUser.java
+++ Interfaces/Repos/IRepoUser.java
@@ -1,4 +1,9 @@
 package Interfaces.Repos;
 
 public interface IRepoUser {
+    boolean isEmailUnique(String email);
+    boolean userExists(String username);
+    public String getEmailWithFormat();
+    boolean isValidEmailFormat(String email);
+    boolean save();
 }
